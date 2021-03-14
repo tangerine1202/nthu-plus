73c0d487caeae872b6e172c387634c6c9b1d37f8
Ice1187 <Icing1187@gmail.com>
2021-03-15 01:31:48 +0800

    Add class schedule with really smelly code

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,66 @@ import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
+function generateSchedule() {
+  let weekday = ['M', 'T', 'W', 'R', 'F'];
+  let class_num = 9;
+  let ret = [
+    <div style={{ 'grid-area': 'dummy', 'border-right': '#eaeaea 2px solid', 'border-bottom': '#eaeaea 2px solid' }}></div>
+  ];
+
+  // generate class index
+  for (let class_ = 1; class_ <= class_num; class_++) {
+    let name = `C${class_}`;
+    let style = {
+      'grid-area': name,
+      'border-right': '#eaeaea 2px solid',
+      'border-bottom': '#eaeaea 2px solid',
+      'display': 'flex',
+      'justify-content': 'center',
+      'align-items': 'center'
+    }
+    ret.push(
+      <div className={name} style={style}>{class_}</div>
+    )
+  }
+
+  for (let day = 0; day < weekday.length; day++) {
+    // generate day
+    let style = {
+      'grid-area': weekday[day],
+      'border-right': '#eaeaea 2px solid',
+      'border-bottom': '#eaeaea 2px solid',
+      'display': 'flex',
+      'justify-content': 'center',
+      'align-items': 'center'
+    }
+    ret.push(
+      <div className={weekday[day]} style={style}>
+        {`${weekday[day]}.`}
+      </div>
+    )
+
+    // generate classes
+    let classes = [];
+    for (let class_ = 1; class_ <= class_num; class_++) {
+      let name = `${weekday[day]}${class_}`;
+      let style = {
+        'grid-area': name,
+        'border-right': '#eaeaea 2px solid',
+        'border-bottom': '#eaeaea 2px solid',
+        'display': 'flex',
+        'justify-content': 'center',
+        'align-items': 'center'
+      }
+      ret.push(
+        <div className={name} style={style}>{name}</div>
+      )
+    }
+  }
+
+  return ret;
+}
+
 export default function Home() {
   return (
     <div>
@@ -18,67 +78,32 @@ export default function Home() {
             {/* this is a gap */}
           </div>
           <Link href="">
-            <a className={styles.headbar_page1}>Page1</a>
+            <a className={styles.headbar_page}>Page1</a>
           </Link>
           <Link href="">
-            <a className={styles.headbar_page2}>Page2</a>
+            <a className={styles.headbar_page}>Page2</a>
           </Link>
           <Link href="">
-            <a className={styles.headbar_page3} > Page3</a>
+            <a className={styles.headbar_page} > Page3</a>
           </Link>
         </nav>
         <main className={styles.main}>
           <div className={styles.left}>
             <button className={styles.college}>
-              Search college here
+              Search college
             </button>
             <button className={styles.department}>
-              Search department here
+              Search department
             </button>
             <button className={styles.unit}>
-              Search unit here
+              Search unit
             </button>
           </div>
           <div className={styles.right}>
-
+            <div className={styles.schedule}>
+              {generateSchedule()}
+            </div>
           </div>
-
-          {/*<h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by editing{' '}
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h3>Documentation &rarr;</h3> <p>Find in-depth information about Next.js features and API.</p> </a>
-             
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h3>Learn &rarr;</h3>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className={styles.card}
-            >
-              <h3>Examples &rarr;</h3>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h3>Deploy &rarr;</h3>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-            </a>
-          </div>*/}
         </main>
 
         <footer className={styles.footer}>
